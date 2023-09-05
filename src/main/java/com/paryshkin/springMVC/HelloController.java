28ce76c33d8ef91725f90a072c96cd3581ac26cb
Mimonstr <p-mishka1@mail.ru>
2023-09-05 12:10:54 +0300

    Configuration with xml code

--- src/main/java/com/paryshkin/springMVC/HelloController.java
+++ src/main/java/com/paryshkin/springMVC/HelloController.java
@@ -1,12 +1,14 @@
 package com.paryshkin.springMVC;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Component
 public class HelloController
 {
-    @GetMapping("/hello-world")
+    @GetMapping("/hello")
     public String sayHello()
     {
         return "hello_world";
