813b7ccf94d2487914a2bbc97e60c95bc28d4a4f
Mimonstr <p-mishka1@mail.ru>
2023-09-05 12:49:47 +0300

    Configuration without xml code (also failed)

--- src/main/java/com/paryshkin/springMVC/HelloController.java
+++ src/main/java/com/paryshkin/springMVC/HelloController.java
@@ -1,14 +1,12 @@
 package com.paryshkin.springMVC;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@Component
 public class HelloController
 {
-    @GetMapping("/hello")
+    @GetMapping("/hello-world")
     public String sayHello()
     {
         return "hello_world";
