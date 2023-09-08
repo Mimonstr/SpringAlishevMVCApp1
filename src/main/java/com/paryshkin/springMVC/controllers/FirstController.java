e1130c6203e185d2d014cadb6ebee9f3510fe59f
Mimonstr <p-mishka1@mail.ru>
2023-09-08 15:06:06 +0300

    Request with parameters and display with html

--- src/main/java/com/paryshkin/springMVC/controllers/FirstController.java
+++ src/main/java/com/paryshkin/springMVC/controllers/FirstController.java
@@ -2,6 +2,7 @@ package com.paryshkin.springMVC.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +15,10 @@ public class FirstController
 {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname)
+                            @RequestParam(value = "surname", required = false) String surname, Model model)
     {
-        System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+       // System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")
