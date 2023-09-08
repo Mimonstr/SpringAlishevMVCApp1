718c4f0cf766b71353cd5f5c197f7d17754afa73
Mimonstr <p-mishka1@mail.ru>
2023-09-08 14:58:51 +0300

    Request with parameters: @RequestParam(value = "name", required = false) String name

--- src/main/java/com/paryshkin/springMVC/controllers/FirstController.java
+++ src/main/java/com/paryshkin/springMVC/controllers/FirstController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //import javax.servlet.http.HttpServletRequest;
 
@@ -12,11 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController
 {
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request)
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname)
     {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-
         System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
