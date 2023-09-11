674a528f52515cc49d6014fd67366793a416ca68
Mimonstr <p-mishka1@mail.ru>
2023-09-11 17:51:26 +0300

    Crud: READ

---
+++ src/main/java/com/paryshkin/springMVC/dao/PersonDAO.java
@@ -0,0 +1,32 @@
+package com.paryshkin.springMVC.dao;
+
+import com.paryshkin.springMVC.models.Person;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO
+{
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Misha"));
+        people.add(new Person(++PEOPLE_COUNT, "Sasha"));
+        people.add(new Person(++PEOPLE_COUNT, "Yulia"));
+        people.add(new Person(++PEOPLE_COUNT, "Kate"));
+    }
+
+    public List<Person> index()
+    {
+        return people;
+    }
+    public Person show(int id)
+    {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
