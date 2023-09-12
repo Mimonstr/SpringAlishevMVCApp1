672d70a78b54ad99a8a62f6f2e787478404cda9f
Mimonstr <p-mishka1@mail.ru>
2023-09-12 14:59:06 +0300

    Crud: Create

--- src/main/java/com/paryshkin/springMVC/controllers/PeopleController.java
+++ src/main/java/com/paryshkin/springMVC/controllers/PeopleController.java
@@ -1,13 +1,11 @@
 package com.paryshkin.springMVC.controllers;
 
 import com.paryshkin.springMVC.dao.PersonDAO;
+import com.paryshkin.springMVC.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -34,5 +32,19 @@ public class PeopleController
         // Получим одного человека по его id из DAO и передадимна отображение в представление
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model)
+    {
+        model.addAttribute("person", new Person());
+
+        return "people/new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person)
+    {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
